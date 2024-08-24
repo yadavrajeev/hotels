cd46646c9402f9a3c6b2a106362a0c12fc500819
yadavrajeev <yadav.rajeev8808@gmail.com>
2024-08-24 23:19:27 +0530

    ||3000added in port removed

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const db = require("./db");
 const app = express();
 // const port = 3000;
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 require('dotenv').config();
 
